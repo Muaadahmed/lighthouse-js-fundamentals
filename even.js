91dbb63242b6cc105442b1bdf0b58191c236e913
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2021-10-06 16:38:09 +0000

    difference beteen return and console.log

--- even.js
+++ even.js
@@ -2,8 +2,13 @@ const isEven = function (num){
     return num % 2 === 0;
 }
 
+// return
 const tenIsEven = isEven(10);
 const elevenIsEven = isEven(11);
 
 console.log(tenIsEven);
 console.log(elevenIsEven);
+
+//console.log
+console.log(isEven(10));
+console.log(isEven(11));
